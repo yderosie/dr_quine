14f8a57185ac43aa9390454864dbbc8e9a775c00
yderosie <yderosie@student.42.fr>
2017-02-15 17:32:18 +0100

    programme 1

---
+++ Colleen.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void second_function();
+/*
+	com1
+*/
+int main(){/* com2 */char*a="#include<stdio.h>%cvoid second_function();%c/*%c%ccom1%c*/%cint main(){/* com2 */char*a=%c%s%c;printf(a,10,10,10,9,10,10,34,a,34);}";printf(a,10,10,10,9,10,10,34,a,34);}

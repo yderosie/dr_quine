0e96c4f38efdfac5bca0775b6ab7185dd379a83d
yderosie <yderosie@student.42.fr>
2017-02-20 18:41:55 +0100

    up

--- Colleen.c
+++ Colleen.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-void second_function();
+void second_function(){}
 /*
 	com1
 */
@@ -8,6 +8,7 @@ int main()
 	/*
 		com2
 	*/
-	char*a="#include<stdio.h>%cvoid second_function();%c/*%c%ccom1%c*/%cint main()%c{%c%c/*%c%c%ccom2%c%c*/%c%cchar*a=%c%s%c;%c%cprintf(a,10,10,10,9,10,10,10,10,9,10,9,9,10,9,10,9,34,a,34,10,9,10);%c}";
-	printf(a,10,10,10,9,10,10,10,10,9,10,9,9,10,9,10,9,34,a,34,10,9,10);
+	char*a="#include<stdio.h>%cvoid second_function(){}%c/*%c%ccom1%c*/%cint main()%c{%c%c/*%c%c%ccom2%c%c*/%c%cchar*a=%c%s%c;%c%csecond_function();%c%cprintf(a,10,10,10,9,10,10,10,10,9,10,9,9,10,9,10,9,34,a,34,10,9,10,9,10);%c}";
+	second_function();
+	printf(a,10,10,10,9,10,10,10,10,9,10,9,9,10,9,10,9,34,a,34,10,9,10,9,10);
 }
